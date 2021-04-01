919bc771a81b5a3561d2c1dd1c475ac200f3a888
Timo <37281330+Timo9Madrid7@users.noreply.github.com>
2021-04-01 22:05:01 +0200

    improve the quality of the code

--- code/java/WangCol_oldVer.java
+++ code/java/WangCol_oldVer.java
@@ -38,6 +38,18 @@ public class WangCol {
 		return X;
 	}
 
+//	32-bit padding
+	private String bit32Pad(int b) {
+		String bStr = Integer.toBinaryString(b);
+		int pad_zero = 32 - bStr.length();
+		if (pad_zero > 0) {
+			for (int i = 0; i < pad_zero; i++) {
+				bStr = "0" + bStr;
+			}
+		}
+		return bStr;
+	}
+
 	private boolean msb_equal(int a, int b) {
 		return (((~a) ^ (b)) & 0x80000000) != 0;
 	}
@@ -117,13 +129,13 @@ public class WangCol {
 			T = RR(Q[7] - Q[6], 17); // T_6
 			if ((T & 0x4000) != 0 || ((T >> 10) & 0xf) == 0) // 0 and 1
 				continue;
-//			x[6] = T - F(Q[6], Q[5], Q[4]) - Q[3] - K[6];
+			x[6] = T - F(Q[6], Q[5], Q[4]) - Q[3] - K[6];
 
 			Q[8] = prg();
 			Q[8] &= 0x01f15540;
 			Q[8] |= 0x01910540;
 			T = RR(Q[8] - Q[7], 22);
-//			x[7] = T - F(Q[7], Q[6], Q[5]) - Q[4] - K[7];
+			x[7] = T - F(Q[7], Q[6], Q[5]) - Q[4] - K[7];
 
 			for (;;) {
 
@@ -136,7 +148,7 @@ public class WangCol {
 					T = RR(Q[9] - Q[8], 7); // T_8
 					if ((T >> 31) == 0 || (T & 0x1000000) == 0) // 1 and 1
 						continue;
-//					x[8] = T - F(Q[8], Q[7], Q[6]) - Q[5] - K[8];
+					x[8] = T - F(Q[8], Q[7], Q[6]) - Q[5] - K[8];
 
 					Q[10] = prg();
 					Q[10] &= 0xff7fde7c;
@@ -145,7 +157,7 @@ public class WangCol {
 					T = RR(Q[10] - Q[9], 12); // T_9
 					if ((~T >> 27) == 0) // 1
 						continue;
-//					x[9] = T - F(Q[9], Q[8], Q[7]) - Q[6] - K[9];
+					x[9] = T - F(Q[9], Q[8], Q[7]) - Q[6] - K[9];
 
 					Q[11] = prg();
 					Q[11] &= 0xbff1cefc;
@@ -158,7 +170,7 @@ public class WangCol {
 
 				} // end for 2
 
-//				x[10] = T - F(Q[10], Q[9], Q[8]) - Q[7] - K[10];
+				x[10] = T - F(Q[10], Q[9], Q[8]) - Q[7] - K[10];
 
 				Q[12] = prg();
 				Q[12] &= 0xbff81f7f;
@@ -171,13 +183,13 @@ public class WangCol {
 
 			} // end for 1
 
-//			x[11] = T - F(Q[11], Q[10], Q[9]) - Q[8] - K[11];
+			x[11] = T - F(Q[11], Q[10], Q[9]) - Q[8] - K[11];
 
 			Q[13] = prg();
 			Q[13] &= 0xfdfffe7f;
 			Q[13] |= 0x410fe008;
 			Q[13] ^= (Q[12] ^ Q[13]) & 0x80000000;
-//			x[12] = RR(Q[13] - Q[12], 7) - F(Q[12], Q[11], Q[10]) - Q[9] - K[12];
+			x[12] = RR(Q[13] - Q[12], 7) - F(Q[12], Q[11], Q[10]) - Q[9] - K[12];
 
 			for (;;) {
 
@@ -185,7 +197,7 @@ public class WangCol {
 				Q[14] &= 0xdcfbffff;
 				Q[14] |= 0x000be188;
 				Q[14] ^= (Q[13] ^ Q[14]) & 0x80000000;
-//				x[13] = RR(Q[14] - Q[13], 12) - F(Q[13], Q[12], Q[11]) - Q[10] - K[13];
+				x[13] = RR(Q[14] - Q[13], 12) - F(Q[13], Q[12], Q[11]) - Q[10] - K[13];
 
 				Q[15] = prg();
 				Q[15] &= 0xfdfffff7;
@@ -193,20 +205,17 @@ public class WangCol {
 				T = RR(Q[15] - Q[14], 17);
 				if ((T >> 30) == 0) // 1
 					continue;
-//				x[14] = T - F(Q[14], Q[13], Q[12]) - Q[11] - K[14];
+				x[14] = T - F(Q[14], Q[13], Q[12]) - Q[11] - K[14];
 
 				Q[16] = prg();
 				Q[16] |= 0x20000000;
 				Q[16] ^= (Q[15] ^ Q[16]) & 0x80000000;
 
 				T = RR(Q[16] - Q[15], 22); // T_15
-//				x[15] = T - F(Q[15], Q[14], Q[13]) - Q[12] - K[15];
+				x[15] = T - F(Q[15], Q[14], Q[13]) - Q[12] - K[15];
 				if ((T & 0x380) != 0 && (~T >> 26) != 0) // 1 and 1
 					break;
 			}
-
-			x[6] = RR(Q[7] - Q[6], 17) - F(Q[6], Q[5], Q[4]) - Q[3] - K[6];
-			x[11] = RR(Q[12] - Q[11], 22) - F(Q[11], Q[10], Q[9]) - Q[8] - K[11];
 
 			for (ctr = 0; ctr < 0xffff; ctr++) {
 
@@ -237,35 +246,21 @@ public class WangCol {
 
 				Q[1] = IVB + RL(IVA + F(IVB, IVC, IVD) + K[0] + x[0], 7);
 				Q[2] = Q[1] + RL(IVD + F(Q[1], IVB, IVC) + K[1] + x[1], 12);
-
-//				x[2] = RR(Q[3] - Q[2], 17) - F(Q[2], Q[1], IVB) - IVC - K[2];
-//				x[3] = RR(Q[4] - Q[3], 22) - F(Q[3], Q[2], Q[1]) - IVB - K[3];
-//				x[4] = RR(Q[5] - Q[4], 7) - F(Q[4], Q[3], Q[2]) - Q[1] - K[4];
-//				x[5] = RR(Q[6] - Q[5], 12) - F(Q[5], Q[4], Q[3]) - Q[2] - K[5];
-//				// x[6] = RR(Q[7] - Q[6], 17) - F(Q[6], Q[5], Q[4]) - Q[3] - K[6];
-//				x[7] = RR(Q[8] - Q[7], 22) - F(Q[7], Q[6], Q[5]) - Q[4] - K[7];
-//				x[8] = RR(Q[9] - Q[8], 7) - F(Q[8], Q[7], Q[6]) - Q[5] - K[8];
-//				x[9] = RR(Q[10] - Q[9], 12) - F(Q[9], Q[8], Q[7]) - Q[6] - K[9];
-//				x[10] = RR(Q[11] - Q[10], 17) - F(Q[10], Q[9], Q[8]) - Q[7] - K[10];
-//				// x[11] = RR(Q[12] - Q[11], 22) - F(Q[11], Q[10], Q[9]) - Q[8] - K[11];
-//				x[12] = RR(Q[13] - Q[12], 7) - F(Q[12], Q[11], Q[10]) - Q[9] - K[12];
-//				x[13] = RR(Q[14] - Q[13], 12) - F(Q[13], Q[12], Q[11]) - Q[10] - K[13];
-//				x[14] = RR(Q[15] - Q[14], 17) - F(Q[14], Q[13], Q[12]) - Q[11] - K[14];
-//				x[15] = RR(Q[16] - Q[15], 22) - F(Q[15], Q[14], Q[13]) - Q[12] - K[15];
+				x[2] = RR(Q[3] - Q[2], 17) - F(Q[2], Q[1], IVB) - IVC - K[2];
+				x[3] = RR(Q[4] - Q[3], 22) - F(Q[3], Q[2], Q[1]) - IVB - K[3];
+				x[4] = RR(Q[5] - Q[4], 7) - F(Q[4], Q[3], Q[2]) - Q[1] - K[4];
+				x[5] = RR(Q[6] - Q[5], 12) - F(Q[5], Q[4], Q[3]) - Q[2] - K[5];
 
 				// check rest
-				x[5] = RR(Q[6] - Q[5], 12) - F(Q[5], Q[4], Q[3]) - Q[2] - K[5];
 				Q[21] = Q[20] + RL(Q[17] + G(Q[20], Q[19], Q[18]) + K[20] + x[5], 5);
 				if (!bitsequal(Q[21], 17, Q[20]) || !msb_equal(Q[21], Q[20])) // checked
 					continue;
 
-				x[10] = RR(Q[11] - Q[10], 17) - F(Q[10], Q[9], Q[8]) - Q[7] - K[10];
 				Q[22] = Q[21] + RL(Q[18] + G(Q[21], Q[20], Q[19]) + K[21] + x[10], 9);
 				if (!msb_equal(Q[22], Q[21])) // checked
 					continue;
 
 				// T_22
-				x[15] = RR(Q[16] - Q[15], 22) - F(Q[15], Q[14], Q[13]) - Q[12] - K[15];
 				T = Q[19] + G(Q[22], Q[21], Q[20]) + K[22] + x[15];
 				if ((T & 0x20000) != 0) // 0
 					continue;
@@ -274,28 +269,20 @@ public class WangCol {
 				if ((Q[23] >> 31) != 0) // 0
 					continue;
 
-				x[4] = RR(Q[5] - Q[4], 7) - F(Q[4], Q[3], Q[2]) - Q[1] - K[4];
 				Q[24] = Q[23] + RL(Q[20] + G(Q[23], Q[22], Q[21]) + K[23] + x[4], 20);
 				if ((Q[24] >> 31) == 0) // 1
 					continue;
-
-				x[2] = RR(Q[3] - Q[2], 17) - F(Q[2], Q[1], IVB) - IVC - K[2];
-				x[3] = RR(Q[4] - Q[3], 22) - F(Q[3], Q[2], Q[1]) - IVB - K[3];
-				x[7] = RR(Q[8] - Q[7], 22) - F(Q[7], Q[6], Q[5]) - Q[4] - K[7];
-				x[8] = RR(Q[9] - Q[8], 7) - F(Q[8], Q[7], Q[6]) - Q[5] - K[8];
-				x[9] = RR(Q[10] - Q[9], 12) - F(Q[9], Q[8], Q[7]) - Q[6] - K[9];
-				x[12] = RR(Q[13] - Q[12], 7) - F(Q[12], Q[11], Q[10]) - Q[9] - K[12];
-				x[13] = RR(Q[14] - Q[13], 12) - F(Q[13], Q[12], Q[11]) - Q[10] - K[13];
-				x[14] = RR(Q[15] - Q[14], 17) - F(Q[14], Q[13], Q[12]) - Q[11] - K[14];
 
 				Q[25] = Q[24] + RL(Q[21] + G(Q[24], Q[23], Q[22]) + K[24] + x[9], 5);
 				Q[26] = Q[25] + RL(Q[22] + G(Q[25], Q[24], Q[23]) + K[25] + x[14], 9);
 				Q[27] = Q[26] + RL(Q[23] + G(Q[26], Q[25], Q[24]) + K[26] + x[3], 14);
 				Q[28] = Q[27] + RL(Q[24] + G(Q[27], Q[26], Q[25]) + K[27] + x[8], 20);
+
 				Q[29] = Q[28] + RL(Q[25] + G(Q[28], Q[27], Q[26]) + K[28] + x[13], 5);
 				Q[30] = Q[29] + RL(Q[26] + G(Q[29], Q[28], Q[27]) + K[29] + x[2], 9);
 				Q[31] = Q[30] + RL(Q[27] + G(Q[30], Q[29], Q[28]) + K[30] + x[7], 14);
 				Q[32] = Q[31] + RL(Q[28] + G(Q[31], Q[30], Q[29]) + K[31] + x[12], 20);
+
 				Q[33] = Q[32] + RL(Q[29] + H(Q[32], Q[31], Q[30]) + K[32] + x[5], 4);
 				Q[34] = Q[33] + RL(Q[30] + H(Q[33], Q[32], Q[31]) + K[33] + x[8], 11);
 
@@ -447,7 +434,7 @@ public class WangCol {
 				T = RR(Q[2] - Q[1], 12);
 				if ((~T >> 25) == 0)
 					continue;
-//				xx[1] = T - D0 - F(Q[1], B0, C0) - K[1];
+				xx[1] = T - D0 - F(Q[1], B0, C0) - K[1];
 
 				Q[3] = prg();
 				Q[3] &= 0xbfdfef7f;
@@ -457,7 +444,7 @@ public class WangCol {
 				T = RR(Q[3] - Q[2], 17);
 				if ((T >> 31) != 0 || ((~T >> 26) & 0x1f) == 0)
 					continue;
-//				xx[2] = T - C0 - F(Q[2], Q[1], B0) - K[2];
+				xx[2] = T - C0 - F(Q[2], Q[1], B0) - K[2];
 
 				Q[4] = prg();
 				Q[4] &= 0xbbc4e611;
@@ -467,7 +454,7 @@ public class WangCol {
 				T = RR(Q[4] - Q[3], 22);
 				if ((T >> 27) == 0)
 					continue;
-//				xx[3] = T - B0 - F(Q[3], Q[2], Q[1]) - K[3];
+				xx[3] = T - B0 - F(Q[3], Q[2], Q[1]) - K[3];
 
 				Q[5] = prg();
 				Q[5] &= 0xcbefee50;
@@ -477,7 +464,7 @@ public class WangCol {
 				T = RR(Q[5] - Q[4], 7);
 				if ((T >> 31) != 0 || (T & 0x40000000) == 0)
 					continue;
-//				xx[4] = T - Q[1] - F(Q[4], Q[3], Q[2]) - K[4];
+				xx[4] = T - Q[1] - F(Q[4], Q[3], Q[2]) - K[4];
 
 				Q[6] = prg();
 				Q[6] &= 0xe5eeec56;
@@ -487,7 +474,7 @@ public class WangCol {
 				T = RR(Q[6] - Q[5], 12);
 				if ((T >> 30) == 0)
 					continue;
-//				xx[5] = T - Q[2] - F(Q[5], Q[4], Q[3]) - K[5];
+				xx[5] = T - Q[2] - F(Q[5], Q[4], Q[3]) - K[5];
 
 				Q[7] = prg();
 				Q[7] &= 0xf7cdfe3f;
@@ -498,7 +485,7 @@ public class WangCol {
 				T = RR(Q[7] - Q[6], 17);
 				if ((T >> 31) != 0 || (T & 0x7c00) == 0)
 					continue;
-//				xx[6] = T - Q[3] - F(Q[6], Q[5], Q[4]) - K[6];
+				xx[6] = T - Q[3] - F(Q[6], Q[5], Q[4]) - K[6];
 
 				Q[8] = prg();
 				Q[8] &= 0xe47efffe;
@@ -508,7 +495,7 @@ public class WangCol {
 				T = RR(Q[8] - Q[7], 22);
 				if ((~T & 0x3f0) == 0 || (~T >> 27) == 0)
 					continue;
-//				xx[7] = T - Q[4] - F(Q[7], Q[6], Q[5]) - K[7];
+				xx[7] = T - Q[4] - F(Q[7], Q[6], Q[5]) - K[7];
 
 				Q[9] = prg();
 				Q[9] &= 0xfc7d7ddd; // Added Q_9[5] = 0 *
@@ -519,7 +506,7 @@ public class WangCol {
 				T = RR(Q[9] - Q[8], 7);
 				if ((T >> 31) == 0 || (~T & 0x7e000000) == 0)
 					continue;
-//				xx[8] = T - Q[5] - F(Q[8], Q[7], Q[6]) - K[8];
+				xx[8] = T - Q[5] - F(Q[8], Q[7], Q[6]) - K[8];
 
 				Q[10] = prg();
 				Q[10] &= 0xfffbeffc;
@@ -531,7 +518,7 @@ public class WangCol {
 				T = RR(Q[10] - Q[9], 12);
 				if ((T >> 27) == 0)
 					continue;
-//				xx[9] = T - Q[6] - F(Q[9], Q[8], Q[7]) - K[9];
+				xx[9] = T - Q[6] - F(Q[9], Q[8], Q[7]) - K[9];
 
 				Q[11] = prg();
 				Q[11] &= 0xfffdefff;
@@ -541,7 +528,7 @@ public class WangCol {
 				T = RR(Q[11] - Q[10], 17);
 				if ((T >> 27) == 0)
 					continue;
-//				xx[10] = T - Q[7] - F(Q[10], Q[9], Q[8]) - K[10];
+				xx[10] = T - Q[7] - F(Q[10], Q[9], Q[8]) - K[10];
 
 				Q[12] = prg();
 				Q[12] &= 0xfff81fff;
@@ -561,12 +548,12 @@ public class WangCol {
 				T = RR(Q[14] - Q[13], 12);
 				if (((T >> 12) & 0xff) == 0)
 					continue;
-//				xx[13] = T - Q[10] - F(Q[13], Q[12], Q[11]) - K[13];
-//
-//				xx[0] = RR(Q[1] - B0, 7) - IVA - F(IVB, IVC, IVD) - K[0];
-//				xx[11] = RR(Q[12] - Q[11], 22) - Q[8] - F(Q[11], Q[10], Q[9]) - K[11];
-//				xx[12] = RR(Q[13] - Q[12], 7) - Q[9] - F(Q[12], Q[11], Q[10]) - K[12];
-//				xx[13] = RR(Q[14] - Q[13], 12) - Q[10] - F(Q[13], Q[12], Q[11]) - K[13];
+				xx[13] = T - Q[10] - F(Q[13], Q[12], Q[11]) - K[13];
+
+				xx[0] = RR(Q[1] - B0, 7) - IVA - F(IVB, IVC, IVD) - K[0];
+				xx[11] = RR(Q[12] - Q[11], 22) - Q[8] - F(Q[11], Q[10], Q[9]) - K[11];
+				xx[12] = RR(Q[13] - Q[12], 7) - Q[9] - F(Q[12], Q[11], Q[10]) - K[12];
+				xx[13] = RR(Q[14] - Q[13], 12) - Q[10] - F(Q[13], Q[12], Q[11]) - K[13];
 
 				break;
 			}
@@ -580,7 +567,8 @@ public class WangCol {
 				T = RR(Q[15] - Q[14], 17);
 				if ((~T >> 30) == 0)
 					continue;
-//				xx[14] = T - Q[11] - F(Q[14], Q[13], Q[12]) - K[14];
+
+				xx[14] = T - Q[11] - F(Q[14], Q[13], Q[12]) - K[14];
 
 				Q[16] = prg();
 				Q[16] &= 0x7fffffff;
@@ -590,13 +578,11 @@ public class WangCol {
 				T = RR(Q[16] - Q[15], 22);
 				if ((T & 0x380) == 0 || (T >> 26) == 0)
 					continue;
-//				xx[15] = T - Q[12] - F(Q[15], Q[14], Q[13]) - K[15];
 
-				xx[1] = RR(Q[2] - Q[1], 12) - D0 - F(Q[1], B0, C0) - K[1];
+				xx[15] = T - Q[12] - F(Q[15], Q[14], Q[13]) - K[15];
 
 				changed = 0;
 				Q[17] = Q[16] + RL(G(Q[16], Q[15], Q[14]) + Q[13] + xx[1] + K[16], 5);
-
 				if (!bitsequal(Q[17], 3, Q[16])) {
 					xx[1] += (((Q[2] >> 10) & 1) != 0) ? -0x40000000 : 0x40000000;
 					Q[2] ^= 0x400;
@@ -627,14 +613,12 @@ public class WangCol {
 					xx[4] = RR(Q[5] - Q[4], 7) - Q[1] - F(Q[4], Q[3], Q[2]) - K[4];
 					xx[5] = RR(Q[6] - Q[5], 12) - Q[2] - F(Q[5], Q[4], Q[3]) - K[5];
 				}
-
 				if (Q[17] >> 31 != 0)
 					continue;
 
 				if ((Q[17] - Q[16]) >> 29 == 0x7)
 					continue;
 
-				xx[6] = RR(Q[7] - Q[6], 17) - Q[3] - F(Q[6], Q[5], Q[4]) - K[6];
 				Q[18] = Q[17] + RL(G(Q[17], Q[16], Q[15]) + Q[14] + K[17] + xx[6], 9);
 				// Seems impossible to correct:
 				if ((Q[18] & 0x20000) == 0)
@@ -658,7 +642,6 @@ public class WangCol {
 					xx[9] = RR(Q[10] - Q[9], 12) - Q[6] - F(Q[9], Q[8], Q[7]) - K[9];
 					xx[10] = RR(Q[11] - Q[10], 17) - Q[7] - F(Q[10], Q[9], Q[8]) - K[10];
 				}
-
 				if (Q[18] >> 31 != 0) {
 					Q[3] ^= 0x400000;
 
@@ -679,9 +662,7 @@ public class WangCol {
 					Q[18] = Q[17] + RL(G(Q[17], Q[16], Q[15]) + Q[14] + K[17] + xx[6], 9);
 				}
 
-				xx[11] = RR(Q[12] - Q[11], 22) - Q[8] - F(Q[11], Q[10], Q[9]) - K[11];
 				Q[19] = Q[18] + RL(Q[15] + G(Q[18], Q[17], Q[16]) + K[18] + xx[11], 14);
-
 				if ((Q[19] & 0x20000) != 0) {
 					Q[11] ^= 0x8;
 					T = RR(Q[11] - Q[10], 17);
@@ -694,12 +675,10 @@ public class WangCol {
 					xx[14] = RR(Q[15] - Q[14], 17) - Q[11] - F(Q[14], Q[13], Q[12]) - K[14];
 					Q[19] = Q[18] + RL(Q[15] + G(Q[18], Q[17], Q[16]) + K[18] + xx[11], 14);
 				}
-
 				if ((Q[19] >> 31) != 0)
 					continue;
 
 				// check rest
-				xx[0] = RR(Q[1] - B0, 7) - IVA - F(IVB, IVC, IVD) - K[0];
 				T = Q[16] + G(Q[19], Q[18], Q[17]) + K[19] + xx[0];
 				if ((T >> 29) == 0)
 					continue;
@@ -707,17 +686,14 @@ public class WangCol {
 				if (Q[20] >> 31 != 0)
 					continue;
 
-				xx[5] = RR(Q[6] - Q[5], 12) - Q[2] - F(Q[5], Q[4], Q[3]) - K[5];
 				Q[21] = Q[20] + RL(Q[17] + G(Q[20], Q[19], Q[18]) + K[20] + xx[5], 5);
 				if (!bitsequal(Q[21], 17, Q[20]) || Q[21] >> 31 != 0)
 					continue;
 
-				xx[10] = RR(Q[11] - Q[10], 17) - Q[7] - F(Q[10], Q[9], Q[8]) - K[10];
 				Q[22] = Q[21] + RL(Q[18] + G(Q[21], Q[20], Q[19]) + K[21] + xx[10], 9);
 				if (Q[22] >> 31 != 0)
 					continue;
 
-				xx[15] = RR(Q[16] - Q[15], 22) - Q[12] - F(Q[15], Q[14], Q[13]) - K[15];
 				T = Q[19] + G(Q[22], Q[21], Q[20]) + K[22] + xx[15];
 				if ((T & 0x20000) != 0)
 					continue; // p = 1/2
@@ -726,28 +702,20 @@ public class WangCol {
 				if (Q[23] >> 31 != 0)
 					continue;
 
-				xx[4] = RR(Q[5] - Q[4], 7) - Q[1] - F(Q[4], Q[3], Q[2]) - K[4];
 				Q[24] = Q[23] + RL(Q[20] + G(Q[23], Q[22], Q[21]) + K[23] + xx[4], 20);
 				if ((Q[24] >> 31) == 0)
 					continue;
-
-				xx[2] = RR(Q[3] - Q[2], 17) - C0 - F(Q[2], Q[1], B0) - K[2];
-				xx[3] = RR(Q[4] - Q[3], 22) - B0 - F(Q[3], Q[2], Q[1]) - K[3];
-				xx[7] = RR(Q[8] - Q[7], 22) - Q[4] - F(Q[7], Q[6], Q[5]) - K[7];
-				xx[8] = RR(Q[9] - Q[8], 7) - Q[5] - F(Q[8], Q[7], Q[6]) - K[8];
-				xx[9] = RR(Q[10] - Q[9], 12) - Q[6] - F(Q[9], Q[8], Q[7]) - K[9];
-				xx[12] = RR(Q[13] - Q[12], 7) - Q[9] - F(Q[12], Q[11], Q[10]) - K[12];
-				xx[13] = RR(Q[14] - Q[13], 12) - Q[10] - F(Q[13], Q[12], Q[11]) - K[13];
-				xx[14] = RR(Q[15] - Q[14], 17) - Q[11] - F(Q[14], Q[13], Q[12]) - K[14];
 
 				Q[25] = Q[24] + RL(Q[21] + G(Q[24], Q[23], Q[22]) + K[24] + xx[9], 5);
 				Q[26] = Q[25] + RL(Q[22] + G(Q[25], Q[24], Q[23]) + K[25] + xx[14], 9);
 				Q[27] = Q[26] + RL(Q[23] + G(Q[26], Q[25], Q[24]) + K[26] + xx[3], 14);
 				Q[28] = Q[27] + RL(Q[24] + G(Q[27], Q[26], Q[25]) + K[27] + xx[8], 20);
+
 				Q[29] = Q[28] + RL(Q[25] + G(Q[28], Q[27], Q[26]) + K[28] + xx[13], 5);
 				Q[30] = Q[29] + RL(Q[26] + G(Q[29], Q[28], Q[27]) + K[29] + xx[2], 9);
 				Q[31] = Q[30] + RL(Q[27] + G(Q[30], Q[29], Q[28]) + K[30] + xx[7], 14);
 				Q[32] = Q[31] + RL(Q[28] + G(Q[31], Q[30], Q[29]) + K[31] + xx[12], 20);
+
 				Q[33] = Q[32] + RL(Q[29] + H(Q[32], Q[31], Q[30]) + K[32] + xx[5], 4);
 				Q[34] = Q[33] + RL(Q[30] + H(Q[33], Q[32], Q[31]) + K[33] + xx[8], 11);
 
@@ -857,14 +825,14 @@ public class WangCol {
 					if ((T & 1) == 0 || (T2 & 1) != 0)
 						continue;
 				}
-				Q[64] = Q[63] + RL(Q[60] + I(Q[63], Q[62], Q[61]) + K[63] + xx[9], 21);
-
+				Q[64] = Q[63] + RL(Q[60]+I(Q[63], Q[62], Q[61]) + K[63] + xx[9], 21);
+				
 				A1 = IVA + Q[61];
 				D1 = IVD + Q[62];
 				C1 = IVC + Q[63];
 				B1 = IVB + Q[64];
-
-				for (int j = 0; j < 16; j++) {
+				
+				for(int j=0;j<16;j++) {
 					Hx[j] = x[j];
 					Hxx[j] = xx[j];
 				}
@@ -874,7 +842,7 @@ public class WangCol {
 				Hxx[4] = xx[4] - 0x80000000;
 				Hxx[11] = xx[11] - 0x00008000;
 				Hxx[14] = xx[14] - 0x80000000;
-
+				
 				return 1;
 			}
 
@@ -896,7 +864,7 @@ public class WangCol {
 
 		System.out.print("Do you want to change the initial IHV? [y/n]: ");
 		IV_flag = scanner.next().charAt(0);
-
+		
 		if (IV_flag == 'y') {
 			System.out.print("Input a new initial IV_0: ");
 			t.A = scanner.nextInt();
@@ -908,17 +876,17 @@ public class WangCol {
 			t.D = scanner.nextInt();
 		} else {
 			System.out.print("It will use the default IHVs: ");
-			System.out.print(Integer.toHexString(t.A) + " " + Integer.toHexString(t.B) + " " + Integer.toHexString(t.C)
-					+ " " + Integer.toHexString(t.D));
+			System.out.print(Integer.toHexString(t.A) + " " + Integer.toHexString(t.B) + " "
+					+ Integer.toHexString(t.C) + " " + Integer.toHexString(t.D));
 		}
-
+		
 		System.out.println();
 		System.out.print("Input an integer as the random seed: ");
 		t.X = scanner.nextInt();
 		System.out.println();
-
+		
 		scanner.close();
-
+		
 		System.out.println("Block 1 Started");
 		long blockStart = System.currentTimeMillis();
 		t.block1();
@@ -926,7 +894,7 @@ public class WangCol {
 		long Timer1 = blockEnd - blockStart;
 		System.out.println("\r\nBlock 1 Finished");
 		System.out.println("Time for the first block:" + Timer1 / 1000 + "sec");
-
+		
 		System.out.println("Block 2 Started");
 		blockStart = System.currentTimeMillis();
 		t.block2();
@@ -934,9 +902,9 @@ public class WangCol {
 		long Timer2 = blockEnd - blockStart;
 		System.out.println("\r\nBlock 2 Finished");
 		System.out.println("Time for the second block:" + Timer2 / 1000 + "sec");
-
+		
 		System.out.println("Total Time:" + (Timer1 + Timer2) / 1000 + "sec");
-
+		
 		String message1 = "";
 		String message2 = "";
 		String hash = toHex(t.A1) + toHex(t.B1) + toHex(t.C1) + toHex(t.D1);
@@ -953,7 +921,7 @@ public class WangCol {
 				message2 += "\r\n";
 			}
 		}
-
+		
 		System.out.println("\n----------------------Collision Found!----------------------\n");
 
 		System.out.println("M1:\n" + message1);
